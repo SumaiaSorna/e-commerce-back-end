eddab0bf90cf398b947a6c171ed35fa066384757
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-15 01:08:16 +0000

    done with all the rotes and catagories controller function

---
+++ src/controllers/api/catagories.js
@@ -0,0 +1,30 @@
+const getAllCatagories = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  res.send("getAllCatagories");
+};
+const getCatagoriesById = (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  res.send("getCatagoriesById");
+};
+const createCatagories = (req, res) => {
+  // create a new category
+  res.send("createCatagories");
+};
+const updateCatagoriesById = (req, res) => {
+  // update a category by its `id` value
+  res.send("updateCatagoriesById");
+};
+const deleteCatagoriesById = (req, res) => {
+  // delete a category by its `id` value
+  res.send("deleteCatagoriesById");
+};
+
+module.exports = {
+  getAllCatagories,
+  getCatagoriesById,
+  createCatagories,
+  updateCatagoriesById,
+  deleteCatagoriesById,
+};
--- src/routes/api/category-routes.js
+++ src/routes/api/category-routes.js
@@ -1,31 +1,46 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const {
+  getAllCatagories,
+  getCatagoriesById,
+  createCatagories,
+  updateCatagoriesById,
+  deleteCatagoriesById,
+} = require("../../controllers/api/catagories");
 
-const { Category, Product } = require('../../models');
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+router.get("/", getAllCatagories);
+router.get("/:id", getCatagoriesById);
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+router.post("/", createCatagories);
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+router.put("/:id", updateCatagoriesById);
+router.delete("/:id", deleteCatagoriesById);
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.get('/', (req, res) => {
+//   // find all categories
+//   // be sure to include its associated Products
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+// router.get('/:id', (req, res) => {
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+// });
+
+// router.post('/', (req, res) => {
+//   // create a new category
+// });
+
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+// });
+
+// router.delete('/:id', (req, res) => {
+//   // delete a category by its `id` value
+// });
 
 module.exports = router;
